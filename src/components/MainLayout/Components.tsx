07abed5b90dfabd4d1b0687b7dd5302102dcbee8
Dennis Mubamba <dennism501@outlook.com>
2021-06-11 01:08:21 +0200

    :construction: created Main container

--- src/components/MainLayout/Components.tsx
+++ src/components/MainLayout/Components.tsx
@@ -1,5 +1,21 @@
 import styled from "styled-components";
 
 export const MainLayout = styled.div`
-    width: 100vh;
+    width: 70%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-family: monospace;
+    font-weight: 100;
+    --grey-100: #e4e9f0;
+    --grey-200: #cfd7e3;
+    --grey-300: #b5c0cd;
+    --grey-800: #3e4e63;
+    --grid-gap: 1px;
+    --day-label-size: 20px;
+    position: relative;
+    background-color: var(--grey-200);
+    border: solid 1px var(--grey-200);
 `;
--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import { MainLayout } from '../components/MainLayout/Components';
+import { CalendarHeader } from '../components/Calender/Components'
+import DaysOfTheWeek from '../components/Calender/DaysOfTheWeek';
+import DaysOfTheMonth from '../components/Calender/DaysOfTheMonth';
 
 const Home: React.FunctionComponent = () => {
+
+    const names: string[] = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+    const days: string[] = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31",]
     return (
         <>
             <MainLayout>
-                <p>Hello world</p>
+                <CalendarHeader>
+                    <DaysOfTheWeek weekDays={names} />
+                </CalendarHeader>
+                <DaysOfTheMonth days={days} />
             </MainLayout>
+
         </>
     );
 }
